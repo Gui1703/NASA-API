22a00f5a0f3e26e6690fc6c3dafdfc7c61cad7f4
Guilherme Raposo <gui.raposo36@gmail.com>
2023-07-01 14:38:46 -0300

    feat: ajustado gitignore

--- dist/service/stationService.js
+++
@@ -1,8 +0,0 @@
-import * as stationsDataBase from "../repositories/stationsRepository.js";
-import * as erro from "../utils/erro.js";
-export async function findStationOrThowErro(stationName) {
-    const station = await stationsDataBase.findByName(stationName);
-    if (!station)
-        throw erro.notFound("Not foud station with this name");
-    return station;
-}
